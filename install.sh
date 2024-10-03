ca3faa235967c094cbfa40f3536e6bca3be45e11
CBF <147460707+CBF2006@users.noreply.github.com>
2024-10-03 17:16:21 -0400

    Show that install.sh is running

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "ResolveDeb1Click v0.1.1"
+
 # Navigate to the script's directory
 cd "$(dirname "$0")" || { echo "Failed to change directory"; exit 1; }
 cd ./scripts || { echo "Scripts directory not found"; exit 1; }
